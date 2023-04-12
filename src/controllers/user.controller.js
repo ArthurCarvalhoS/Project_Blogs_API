44aa143fd7fa08caa32a0914857356bf85f78272
Arthur Carvalho <arthurcrsouto@gmail.com>
2023-04-12 20:12:07 -0300

    correção total de lint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,17 +16,13 @@ const getUserById = async (req, res) => {
 };
 
 const registerUser = async (req, res) => {
-        const emailValidation = /\S+@\S+\.\S+/;
+    const emailValidation = /\S+@\S+\.\S+/;
         const { displayName, email, password } = req.body;
         if (displayName.length < 8) {
-            return res.status(400).json( 
-                { message: '"displayName" length must be at least 8 characters long' },
-                );
+return res.status(400).json({ message: '"displayName" length must be at least 8 characters long' });
         }
         if (password.length < 6) {
-            return res.status(400).json( 
-                { message: '"password" length must be at least 6 characters long' },
-                );
+return res.status(400).json({ message: '"password" length must be at least 6 characters long' });
         }
         const exists = await userService.getByEmail(email);
         if (exists) {
